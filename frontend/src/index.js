002807e633ee100ac682e3b052cfd46a18adb2cd
Claudio Godoy <clouds83@gmail.com>
2023-08-24 15:20:54 -0300

    front back connected

---
+++ frontend/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+import './assets/styles/bootstrap.custom.css'
+import './assets/styles/index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index="true" path="/" element={<HomePage />}></Route>
+      <Route path="/product/:id" element={<ProductPage />}></Route>
+    </Route>
+  )
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
+
+reportWebVitals()
